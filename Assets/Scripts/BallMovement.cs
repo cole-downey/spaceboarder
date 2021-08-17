5acff194323306d06b2af18c9b7a0c4bd2fc3021
Colton Downey <cdowney@healthwise.org>
2021-08-17 16:03:33 -0500

    added mulitple sampling points on board

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BallMovement : MonoBehaviour {
     public GameObject character;
     public CharacterMovement characterMovement;
+    public Transform[] groundSamplers;
 
     public float gravStrength = 9.8f;
     private Vector3 gravDirection;
@@ -14,7 +15,7 @@ public class BallMovement : MonoBehaviour {
 
     private Rigidbody rb;
     public bool isGrounded;
-    private float groundedDistance = 0.2f;
+    public float groundedDistance = 0.12f;
     private int layerMask;
     public Vector3 surfaceNor;
 
@@ -23,26 +24,39 @@ public class BallMovement : MonoBehaviour {
         characterMovement = character.GetComponent<CharacterMovement>();
         gravDirection = new Vector3(0.0f, -1.0f, 0.0f);
         layerMask = 1 << 6;
+        groundSamplers = new Transform[4];
+        groundSamplers[0] = character.transform.Find("GroundSamplerFR");
+        groundSamplers[1] = character.transform.Find("GroundSamplerFL");
+        groundSamplers[2] = character.transform.Find("GroundSamplerBR");
+        groundSamplers[3] = character.transform.Find("GroundSamplerBL");
     }
 
     void FixedUpdate() {
+        rb.AddForce(gravDirection * gravStrength, ForceMode.Acceleration);
         if (UpdateGrounded()) {
             //characterMovement.OrientToGround(surfaceNor);
-            
-            rb.AddForce(gravDirection * gravStrength, ForceMode.Acceleration);
             rb.AddForce(Vector3.forward * forwardForce);
             // carving force
             rb.AddForce(character.transform.right * -Vector3.Dot(character.transform.right, rb.velocity) * carveForce);
         } else {
-            rb.AddForce(gravDirection * gravStrength);
         }
 
     }
 
     bool UpdateGrounded() {
         RaycastHit hit;
-        isGrounded = Physics.Raycast(transform.position, gravDirection, out hit, groundedDistance + transform.localScale.x * 0.5f, layerMask);
-        surfaceNor = hit.normal.normalized;
+        bool tempGrounded = false;
+        Vector3 tempNor = Vector3.zero;
+        foreach (var sampler in groundSamplers) {
+            bool thisGrounded = Physics.Raycast(sampler.position, sampler.forward, out hit, groundedDistance, layerMask);
+            if(thisGrounded) {
+                tempGrounded = true;
+                tempNor += hit.normal;
+            }
+        }
+        //isGrounded = Physics.Raycast(transform.position, gravDirection, out hit, groundedDistance + transform.localScale.x * 0.5f, layerMask);
+        isGrounded = tempGrounded;
+        surfaceNor = tempNor.normalized;
         return isGrounded;
     }
 
